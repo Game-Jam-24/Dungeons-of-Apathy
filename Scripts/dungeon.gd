55ab23e7a66d0c4b8b8cc121f4c80d02ce07c1d5
Spoons <81588859+SpoonishBard@users.noreply.github.com>
2024-01-13 18:21:20 -0500

    tinkering with Godot tilemaps and walker procedural.

---
+++ Scripts/dungeon.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var borders = Rect2(1,1,40,22)
+@onready var tileMap = $TileMap
+
+func _ready():
+  randomize()
+  generate_level()
+
+func generate_level():
+  var walker = Walker.new(borders.get_center(), borders)
+  var map = walker.walk(500)
+  walker.queue_free()
+  tileMap.set_cells_terrain_connect(0,map,0,-1)
+
+func _input(event):
+  if event.is_action_pressed("ui_accept") and GlobalVariables.DEBUG:
+    get_tree().reload_current_scene()
