0703d005cb339b1006446327fb644ad444789c47
Makerizms <144258738+Makerizms@users.noreply.github.com>
2024-01-14 18:09:38 +0100

    Fixed tabs in walker scripts

--- Scripts/dungeon.gd
+++ Scripts/dungeon.gd
@@ -4,15 +4,15 @@ var borders = Rect2(1,1,40,22)
 @onready var tileMap = $TileMap
 
 func _ready():
-  randomize()
-  generate_level()
+	randomize()
+	generate_level()
 
 func generate_level():
-  var walker = Walker.new(borders.get_center(), borders)
-  var map = walker.walk(500)
-  walker.queue_free()
-  tileMap.set_cells_terrain_connect(1,map,0,-1)
+	var walker = Walker.new(borders.get_center(), borders)
+	var map = walker.walk(500)
+	walker.queue_free()
+	tileMap.set_cells_terrain_connect(1,map,0,-1)
 
 func _input(event):
-  if event.is_action_pressed("ui_accept") and GlobalVariables.DEBUG:
-	get_tree().reload_current_scene()
+	if event.is_action_pressed("ui_accept") and GlobalVariables.DEBUG:
+		get_tree().reload_current_scene()
